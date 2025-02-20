4e586b1aa666c0ed16c7de3dc3bd23ce7b6d1ce2
shevchenko-nikita <nicksevchenkos@gmail.com>
2025-02-20 23:23:27 +0200

    extract repository Mark methods into separate file

--- backend/internals/repository/assignments.go
+++ backend/internals/repository/assignments.go
@@ -31,34 +31,6 @@ func DeleteAssignment(db *sql.DB, assignmentId int) error {
 	return err
 }
 
-func AddNewMark(db *sql.DB, mark models.Mark) error {
-	query := "INSERT INTO marks (assignment_id, student_id, mark) VALUES(?, ?, ?)"
-
-	_, err := db.Exec(query, mark.AssignmentId, mark.StudentId, mark.Mark)
-
-	return err
-}
-
-func UpdateMark(db *sql.DB, mark models.Mark) error {
-	query := "UPDATE marks SET mark = ? WHERE assignment_id = ? AND student_id = ?"
-
-	_, err := db.Exec(query, mark.Mark, mark.AssignmentId, mark.StudentId)
-
-	return err
-}
-
-func MarkAlreadyExist(db *sql.DB, mark models.Mark) bool {
-	query := "SElECT EXISTS (SELECT * FROM marks WHERE assignment_id = ? AND student_id = ?)"
-
-	var alreadyExist bool
-
-	if err := db.QueryRow(query, mark.AssignmentId, mark.StudentId).Scan(&alreadyExist); err != nil {
-		return false
-	}
-
-	return alreadyExist
-}
-
 func GetAssignmentByID(db *sql.DB, assignmentId int) (models.Assignment, error) {
 	var assignment models.Assignment
 
---
+++ backend/internals/repository/mark.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"database/sql"
+	"github.com/shevchenko-nikita/EDiary/internals/models"
+)
+
+func AddNewMark(db *sql.DB, mark models.Mark) error {
+	query := "INSERT INTO marks (assignment_id, student_id, mark) VALUES(?, ?, ?)"
+
+	_, err := db.Exec(query, mark.AssignmentId, mark.StudentId, mark.Mark)
+
+	return err
+}
+
+func UpdateMark(db *sql.DB, mark models.Mark) error {
+	query := "UPDATE marks SET mark = ? WHERE assignment_id = ? AND student_id = ?"
+
+	_, err := db.Exec(query, mark.Mark, mark.AssignmentId, mark.StudentId)
+
+	return err
+}
+
+func MarkAlreadyExist(db *sql.DB, mark models.Mark) bool {
+	query := "SElECT EXISTS (SELECT * FROM marks WHERE assignment_id = ? AND student_id = ?)"
+
+	var alreadyExist bool
+
+	if err := db.QueryRow(query, mark.AssignmentId, mark.StudentId).Scan(&alreadyExist); err != nil {
+		return false
+	}
+
+	return alreadyExist
+}
