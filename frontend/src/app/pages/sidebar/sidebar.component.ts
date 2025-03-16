1a475b2700313193e06b4c1f3e37014d591e34f0
shevchenko-nikita <nicksevchenkos@gmail.com>
2025-03-16 14:06:07 +0200

    fix sidebar hide functionality

--- frontend/src/app/pages/sidebar/sidebar.component.ts
+++ frontend/src/app/pages/sidebar/sidebar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+	selector: 'app-sidebar',
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
-  
+export class SidebarComponent implements AfterViewInit {
+	@ViewChild('sidebar') sidebar!: ElementRef;
+
+	constructor(private renderer: Renderer2) {}
+
+	ngAfterViewInit(): void {
+		this.renderer.addClass(this.sidebar.nativeElement, 'close'); // Закрываем по умолчанию
+	}
+
+	hideSidebar(): void {
+		this.renderer.addClass(this.sidebar.nativeElement, 'close');
+	}
+
+	showSidebar(): void {
+		this.renderer.removeClass(this.sidebar.nativeElement, 'close');
+	}
 }
