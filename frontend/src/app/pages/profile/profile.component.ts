a9423c3d81ff6fd19b1f4932f5ff25456b56ced0
shevchenko-nikita <nicksevchenkos@gmail.com>
2025-03-16 14:56:07 +0200

    add profile functionality in frontend

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
 	selector: 'app-profile',
@@ -11,18 +12,40 @@ export class ProfileComponent {
 	profileForm: FormGroup;
 	profileImage: string = '';
   
-	constructor(private fb: FormBuilder) {
+	constructor(private fb: FormBuilder, private http: HttpClient) {
 	  this.profileForm = this.fb.group({
-		firstName: [''],
-		fatherName: [''],
-		lastName: [''],
+		first_name: [''],
+		middle_name: [''],
+		second_name: [''],
 		username: [''],
 		email: ['']
 	  });
 	}
+
+	ngOnInit() {
+		this.loadProfile();
+	}
+
+	loadProfile() {
+		this.http.get<any>('http://localhost:8080/user/profile', { withCredentials: true }).subscribe(data => {
+			this.profileForm.patchValue({
+				first_name: data.first_name,
+				middle_name: data.middle_name,
+				second_name: data.second_name,
+				username: data.username,
+				email: data.email
+		  	});
+		  	
+			const baseURL = 'http://localhost:8080/';
+			this.profileImage = baseURL + data.profile_img_path;
+			console.log(this.profileImage);
+		});
+	}
   
 	onSubmit() {
-	  console.log(this.profileForm.value);
+		this.http.put('http://localhost:8080/user/update-profile', this.profileForm.value, { withCredentials: true }).subscribe(response => {
+			console.log('Профіль оновлено', response);
+		});
 	}
   
 	changeImage() {
