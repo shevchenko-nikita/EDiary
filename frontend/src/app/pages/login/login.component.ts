022724f2fb5177f8abacec5cc0bbbb8d49152e34
shevchenko-nikita <nicksevchenkos@gmail.com>
2025-05-22 21:16:11 +0300

    show password in login page

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -13,21 +13,23 @@ export class LoginComponent
 	username = '';
 	password = '';
 	errorMessage = '';
+	showPassword = false;
 
 	constructor(private http: HttpClient, private router: Router) {}
 
+	togglePasswordVisibility() {
+		this.showPassword = !this.showPassword;
+	}
+
 	onLogin() {
 		this.errorMessage = '';
-		console.log('Вхідні дані:', { username: this.username, password: this.password });
 		const loginData = { username: this.username, password: this.password };
 
 		this.http.post('http://localhost:8080/sign-in', loginData, { withCredentials: true }).subscribe({
 			next: () => {
-				console.log('Авторизація успішна!');
 				this.router.navigate(['']);
 			},
 			error: (err) => {
-				console.error('Помилка авторизації:', err);
 				this.errorMessage = 'Невірний username або пароль!';
 			},
 		});
